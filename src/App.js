443d89ed5456413f0a559d61eb53add1002bfd1d
JoaoCnh <joaomarinho.cunha@gmail.com>
2017-12-19 17:27:33 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
-import './App.css'
+import React, { Component } from "react";
+import styled from "styled-components";
+import FaSpotify from "react-icons/lib/fa/spotify";
 
-import React, {Component} from 'react'
+const Container = styled.div`
+  display: flex;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const AuthBox = styled.div`
+  width: 300px;
+  padding: 20px;
+  text-align: center;
+`;
+
+const SpotifyIcon = styled(FaSpotify)`
+  fill: #1db954;
+`;
+
+const AuthButton = styled.button`
+  border: none;
+  outline: none;
+  padding: 0.75em 1.5em;
+  font-weight: 400;
+  border-radius: 4px;
+  text-align: center;
+`;
 
 class App extends Component {
   render() {
-    return <div className="App">
-      <div className="App-heading App-flex">
-        <h2>Welcome to <span className="App-react">React</span></h2>
-      </div>
-      <div className="App-instructions App-flex">
-        <img className="App-logo" src={require('./react.svg')}/>
-        <p>Edit <code>src/App.js</code> and save to hot reload your changes.</p>
-      </div>
-    </div>
+    return (
+      <Container>
+        <AuthBox>
+          <AuthButton type="button" onClick={() => {}}>
+            Login with Spotify
+          </AuthButton>
+        </AuthBox>
+      </Container>
+    );
   }
 }
 
-export default App
+export default App;
